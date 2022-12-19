6569cf504d6d518cde347a26766fd9c40701a0c5
srujith <srujithreddy801@gmail.com>
2022-12-19 13:30:17 +0530

    New files added

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pandas as pd
 app=Flask(__name__)
 
 classify=pickle.load(open('classify.pkl','rb'))
+processdata=pickle.load(open('pipeline.pkl','rb'))
 
 def home():
     return render_template('home.html')
@@ -14,13 +15,14 @@ def home():
 def predict_api():
     data=request.json['data']
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
+    data=processdata.transform(data)
     output=classify.predict(data)
     print(output[0])
     return jsonify(output[0])
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['POST']) 
 def predict():
     data=[float(x) for x in request.form.values()] 
+    data=processdata.transform(data)
     output=classify.predict(data)[0]
     return render_template("home.html",prediction_text="The song will {}".format(output))
